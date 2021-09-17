826d5439873cf5de7256ef6259ad9ec67f2cde30
ice2me <ice2me1989@gmail.com>
2021-09-17 13:10:59 +0300

    deploy

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/api/recipe", require("./routes/recipe.route"));
+
+
 
 if (process.env.NODE_ENV === "prod") {
 	app.use("/", express.static(path.join(__dirname, "client", "build")));
@@ -21,8 +25,8 @@ if (process.env.NODE_ENV === "prod") {
 	});
 }
 
-const PORT = config.get("port") || 5000;
-// const PORT = Process.env.PORT || 5000
+// const PORT = config.get("port") || 5000;
+
 
 async function start() {
 	console.log(`Server started on port: ${PORT}...***********************`)
