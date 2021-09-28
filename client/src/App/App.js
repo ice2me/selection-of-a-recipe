12c3eaaf56d60b666fbac805ae19b5227c4e8ddd
ice2me <ice2me1989@gmail.com>
2021-09-28 17:24:38 +0300

    add number of recipes

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import InputEnter from "../components/InputEnter";
 import logo from '../imgaes/logo.png'
+import InputEnter from "../components/InputEnter";
+
 
 function App() {
-	// if (navigator.serviceWorker) {
-	// 	try {
-	// 		const reg = navigator.serviceWorker.register('../../sw.js')
-	// 		console.log('SW register success', reg)
-	// 	} catch (e) {
-	// 		console.log('SW register fail')
-	// 	}
-	//
-	// }
+	const [screenSaver, setScreenSaver] = useState(true)
+	setTimeout(() => {
+		setScreenSaver(false)
+	}, 2500)
 	return (
 		<div className='App'>
-			<h1>
-				<span><img src={logo} alt="logo" style={{width: '35px', margin: '0 10px 0 0'}}/></span>
-				Рецепты
-				<span><img src={logo} alt="logo" style={{width: '35px', margin: '0 0 0 10px'}}/></span>
-			</h1>
-			<InputEnter/>
-			<p className='pt-3 copyright'>copyright © 2021 version 0.1</p>
+			{
+				screenSaver ?
+					<div
+						style={{
+							width: '100%',
+							height: '100%',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+							flexDirection: 'column'
+						}}>
+						<>
+							<h3>Выберите ингредиенты которые у вас есть и </h3>
+							< img className='firstScreenLogo' src={logo} alt="logo"
+								  style={{width: '235px', margin: '10px auto 0'}}/>
+							<h3>посмотрите что из них можно приготовить &#10155;</h3>
+						</>
+					</div>
+					:
+					<>
+						<h1>
+							<span><img src={logo} alt="logo" style={{width: '35px', margin: '0 10px 0 0'}}/></span>
+							Рецепты
+							<span><img src={logo} alt="logo" style={{width: '35px', margin: '0 0 0 10px'}}/></span>
+						</h1>
+						<InputEnter/>
+						<p className='pt-3 copyright'>copyright © 2021 version 0.1</p>
+					</>
+				
+			}
 		</div>
-	);
+	)
 }
 
 export default App;
