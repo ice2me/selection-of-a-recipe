c07fbf837868cd2a7c979bed9eebd27154e36037
ice2me <ice2me1989@gmail.com>
2021-10-05 19:14:51 +0300

    fix

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import './App.css';
-import logo from '../imgaes/logo.png'
 import InputEnter from "../components/InputEnter";
 import Onload from "../components/Onload/Onload";
 
@@ -17,23 +16,7 @@ function App() {
 					:
 					<>
 						<h1>
-							<span>
-								<img
-									src={logo}
-									alt="logo"
-									style={{width: '40px', margin: '0 10px 0 0'}}
-									className="rotatinAnimationRight"
-								/>
-							</span>
 							Рецепты по ингредиентам
-							<span>
-								<img
-									src={logo}
-									alt="logo"
-									style={{width: '40px', margin: '0 0 0 10px'}}
-									className="rotatinAnimationLeft"
-								/>
-							</span>
 						</h1>
 						<InputEnter />
 						<p className="pt-3 copyright">copyright PepperNode © 2021 version 0.5</p>
